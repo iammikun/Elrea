86cda1f4a219f48efae4373abeeb915a6661d8ac
mikun <timilehinonifara@gmail.com>
2023-06-07 13:35:53 -0700

    sixth commit

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import Nav from "../HomeSection/Nav/Nav";
 import classes from "./Header.module.css";
 import { Link } from "react-router-dom";
 import { GiHamburgerMenu } from "react-icons/gi";
 
 const Header = () => {
+  const [open, setOpen] = useState(false);
+
   return (
     <div className={classes.wrapper}>
       <div className={classes.headerContainer}>
@@ -14,6 +16,33 @@ const Header = () => {
           <GiHamburgerMenu className={classes.btn} />
         </div>
       </div>
+      <nav className={classes.mobileNav}>
+        <ul className={classes.navUl}>
+          <li>
+            <Link to="/">Home</Link>
+            <hr className={classes.span} />
+          </li>
+          <li>
+            <Link to="/portfolio">Portfolio</Link>
+            <hr className={classes.span} />
+          </li>
+          <li>
+            <Link to="/team">Team</Link>
+            <hr className={classes.span} />
+          </li>
+          <li>
+            <Link to="/partners">Partners</Link>
+            <hr className={classes.span} />
+          </li>
+          <li>
+            <Link to="/about">About us</Link>
+            <hr className={classes.span} />
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+        </ul>
+      </nav>
     </div>
   );
 };
