5bbde1c871e553b3b8eb5f37b055a8fd17a6cae9
mikun <timilehinonifara@gmail.com>
2023-06-08 12:02:17 -0700

    sixth commit

--- src/Components/TeamSection/TeamHero/Hero.jsx
+++ src/Components/TeamSection/TeamHero/Hero.jsx
@@ -1,18 +1,24 @@
 import React from "react";
 import classes from "./Hero.module.css";
+import teamImg from "../../../Asset/Images/teamImg.jpg";
+import teamImgTwo from "../../../Asset/Images/teamImgTwo.jpg";
 
 const Hero = () => {
   return (
     <div className={classes.container}>
-      <div className={classes.wrapper}>
-        <h1>
-          We are team of different <br /> talented for one purpose.
-        </h1>
-        <p>
-          Lorem ipsum dolor sit amet, consetetur sadipscing elitr, <br /> sed
-          diam nonumy eirmod tempor invidunt ut labore et <br /> dolore magna
-          aliquyam erat, sed diam voluptua.
-        </p>
+      <div className={classes.contentContainer}>
+        <div className={classes.wrapper}>
+          <h1>We are team of different talented for one purpose.</h1>
+          <p>
+            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+            nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
+            erat, sed diam voluptua.
+          </p>
+          <img src={teamImgTwo} alt="" />
+        </div>
+        <div className={classes.teamImg}>
+          <img src={teamImg} alt="" />
+        </div>
       </div>
     </div>
   );
