d90e43bd72341bf949b01510b3d1938c3614492a
mikun <timilehinonifara@gmail.com>
2023-08-04 08:03:11 -0700

    ajnem

--- src/Components/HomeSection/Hero/Hero.jsx
+++ src/Components/HomeSection/Hero/Hero.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import classes from "./Hero.module.css";
+import Lottie from "lottie-react";
+import animationData from "../../../Asset/roller.json";
 
 const Hero = () => {
   return (
@@ -8,6 +10,7 @@ const Hero = () => {
         <div className={classes.heroText}>
           <h1>Together, we are transforming the world, one idea at a time.</h1>
         </div>
+        <Lottie animationData={animationData} className={classes.lottie} />
       </div>
       <div className={classes.heroImg}>.</div>
     </div>
